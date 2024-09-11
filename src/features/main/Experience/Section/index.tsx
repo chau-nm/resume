b54fbdbbc03fc453a65005ae71a475d35c56ee05
chau-nm <chaunm.146.2001@gmail.com>
2024-09-11 22:17:21 +0700

    update experience first

---
+++ src/features/main/Experience/Section/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Section: FC = () => {
+  return <>section</>;
+};
+
+export default Section;
---
+++ src/features/main/Experience/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Experience: FC = () => {
+  return <>Experience</>;
+};
+
+export default Experience;
