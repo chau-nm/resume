44952f849b0c5b653ecf0bf0f54098f294f7104a
chau-nm <chaunm.146.2001@gmail.com>
2024-09-15 00:41:40 +0700

    update ui problem project

--- src/feature/main/Repository/Items/UIProblem.tsx
+++
@@ -1,5 +0,0 @@
-import React, { FC } from "react";
-
-export const UIProblem: FC = () => {
-  return <div>UIProblem</div>;
-};
---
+++ src/feature/main/Repository/Items/UIProblem/index.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from "react";
+import styles from "./uiProblem.module.scss";
+import { CSSLogo, HTMLLogo, JavaScriptLogo } from "assets/images/logo";
+
+export const UIProblem: FC = () => {
+  const technicalUrls = [HTMLLogo, CSSLogo, JavaScriptLogo];
+
+  return (
+    <div className={styles["ui-problem"]}>
+      <div className={styles["title"]}>
+        <h3>UI Problem</h3>
+      </div>
+      <div className={styles["content"]}>
+        <ul>
+          <li className={styles["field"]}>
+            <label>Summary: </label> A project designed to elevate my frontend
+            development skills.
+          </li>
+          <li className={styles["field"]}>
+            <label>Github: </label>{" "}
+            <a href="https://github.com/chau-nm/ui_problems">
+              https://github.com/chau-nm/ui_problems
+            </a>
+          </li>
+          <li className={styles["field"]}>
+            <label>Technical: </label>{" "}
+            {technicalUrls.map((url) => (
+              <img src={url} alt="" />
+            ))}
+          </li>
+          <li className={styles["field"]}>
+            <p>At this project, I only coding with HTML, CSS, JS.</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
