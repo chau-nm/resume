85574aca974f85b0b49896e26240246381a2677a
chau-nm <chaunm.146.2001@gmail.com>
2024-09-14 19:04:08 +0700

    update logo

--- src/assets/images/logo/index.ts
+++ src/assets/images/logo/index.ts
@@ -1,4 +1,24 @@
 export { default as FujinetLogo } from "./fujinet.jpg";
 
+export { default as HTMLLogo } from "./html.png";
+export { default as CSSLogo } from "./css.png";
+export { default as JavaScriptLogo } from "./js.png";
+export { default as TypeScriptLogo } from "./ts.png";
+
+export { default as JavaLogo } from "./java.png";
+export { default as PythonLogo } from "./python.png";
+export { default as PhpLogo } from "./Php.png";
+
+export { default as ReactLogo } from "./react.png";
+export { default as NextJSLogo } from "./nextjs.png";
+export { default as NestJSLogo } from "./nest.png";
+export { default as Yii2Logo } from "./yii2.png";
 export { default as SpringLogo } from "./spring.png";
 export { default as ThymeleafLogo } from "./thymeleaf.png";
+
+export { default as MySQLLogo } from "./mysql.png";
+export { default as PostgreLogo } from "./postgre.png";
+
+export { default as RedisLogo } from "./redis.png";
+export { default as FirebaseLogo } from "./firebase.png";
+export { default as SamlLogo } from "./saml.png";
