18224337410ffba9a7724d42c6d45bce2cbd6a5b
chau-nm <chaunm.146.2001@gmail.com>
2024-09-15 13:37:40 +0700

    update page not found

--- src/feature/404/index.tsx
+++ src/feature/404/index.tsx
@@ -1,7 +1,16 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
+import styles from "./pageNotFound.module.scss";
 
 const PageNotFound: FC = () => {
-  return <>404</>;
+  return (
+    <div className={styles["page-not-found"]}>
+      <h1>404</h1>
+      <span>Page Fot Found</span>
+      <br />
+      <Link to={"/"}>Go Home</Link>
+    </div>
+  );
 };
 
 export default PageNotFound;
