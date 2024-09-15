6d160d57178b65d300044da295c1d7a4fab8f51f
chau-nm <chaunm.146.2001@gmail.com>
2024-09-15 15:08:59 +0700

    remove 404

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Main from "./feature/main";
-import PageNotFound from "./feature/404";
+// import PageNotFound from "./feature/404";
 
 const App: FC = () => {
   return (
     <Routes>
       <Route path="/:section?" element={<Main />} />
-      <Route path="*" element={<PageNotFound />} />
+      {/* <Route path="*" element={<PageNotFound />} /> */}
     </Routes>
   );
 };
