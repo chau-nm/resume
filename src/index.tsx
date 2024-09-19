5f00286acf74502ee5d762bfe47104873433cc02
chau-nm <chaunm.146.2001@gmail.com>
2024-09-19 22:12:04 +0700

    update hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/resume">
+    <HashRouter basename="/resume">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
