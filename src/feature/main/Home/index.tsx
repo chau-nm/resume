69dc51925b329ac41dd177991a0d188005056c0e
chau-nm <chaunm.146.2001@gmail.com>
2024-09-28 23:50:12 +0700

    update home

--- src/feature/main/Home/index.tsx
+++ src/feature/main/Home/index.tsx
@@ -1,20 +1,35 @@
-import { FC } from "react";
-import styles from "./home.module.scss";
-import { ContactLinnk } from "./ContactLink";
+import { CaretDownOutLine } from "assets/icons";
+import IllustrationCoder from "assets/images/illustration_coder.png";
 import NextSectionButton from "component/NextSectionButton";
+import { FC } from "react";
+import { Link } from "react-scroll";
+import { SUMMARY } from "../sections";
+import { ContactLinnk } from "./ContactLink";
+import styles from "./home.module.scss";
 
 const Home: FC = () => (
   <div id="home" className={styles.home}>
-    <div className={styles.wrapper}>
-      <div className={styles.fname}>
-        <span>NGUYEN MINH CHAU</span>
+    <div className={styles.content}>
+      <div className={styles.left}>
+        <div className={styles.greeting}>
+          <span>👋 I'm N.M.Chau</span>
+        </div>
+        <div className={styles.position}>
+          <span>👉 Software Engineer 👈</span>
+        </div>
+        <ContactLinnk />
+        <div className={styles["explore-button"]}>
+          <Link to={SUMMARY} smooth duration={500}>
+            <CaretDownOutLine />
+            Explore Now
+          </Link>
+        </div>
       </div>
-      <div className={styles.position}>
-        <span>Software Engineer</span>
+      <div className={styles.right}>
+        <img src={IllustrationCoder} alt="" />
       </div>
-      <ContactLinnk />
-      <NextSectionButton title="SUMMARY" path={"summary"} />
     </div>
+    <NextSectionButton title="SUMMARY" path={"summary"} />
   </div>
 );
 
