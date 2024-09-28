f22aeb8b4ad98dd2ce7d10f3a6af82c97b094646
chau-nm <chaunm.146.2001@gmail.com>
2024-09-28 19:26:25 +0700

    update album

---
+++ src/assets/images/album/index.ts
@@ -0,0 +1,1 @@
+export { default as FCode } from "./fcode.jpg";
--- src/feature/main/Album/data.ts
+++ src/feature/main/Album/data.ts
@@ -1,3 +1,9 @@
+import { FCode } from "assets/images/album";
 import { AlbumType } from "./type";
 
-export const albumData: AlbumType[] = [];
+export const albumData: AlbumType[] = [
+  {
+    title: "Fujinet FCode Challenge",
+    src: FCode
+  }
+];
