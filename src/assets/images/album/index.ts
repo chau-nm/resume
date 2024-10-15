0c0b802a96cbef841c0d1a51ae57e5f3e1d4a02d
chau-nm <chaunm.146.2001@gmail.com>
2024-10-15 20:21:21 +0700

    update album

--- src/assets/images/album/index.ts
+++ src/assets/images/album/index.ts
@@ -1,1 +1,4 @@
 export { default as FCode } from "./fcode.jpg";
+export { default as FVungTau1} from "./fjs_vung_tau_1.jpg";
+export { default as FVungTau2} from "./fjs_vung_tau_2.jpeg";
+export { default as FVungTau3} from "./fjs_vung_tau_3.jpeg";
--- src/feature/main/Album/data.ts
+++ src/feature/main/Album/data.ts
@@ -1,9 +1,21 @@
-import { FCode } from "assets/images/album";
+import { FCode, FVungTau1, FVungTau2, FVungTau3 } from "assets/images/album";
 import { AlbumType } from "./type";
 
 export const albumData: AlbumType[] = [
   {
     title: "Fujinet FCode Challenge",
     src: FCode
-  }
+  },
+  {
+    title: "Fujinet Vung Tau 1",
+    src: FVungTau1
+  },
+  {
+    title: "Fujinet Vung Tau 2",
+    src: FVungTau2
+  },
+  {
+    title: "Fujinet Vung Tau 3",
+    src: FVungTau3
+  },
 ];
