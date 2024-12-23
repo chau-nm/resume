d984c8a88c486f629ac1a3e330d92f32c869157d
Nguyễn Minh Châu <chaunm.146.2001@gmail.com>
2024-12-23 19:57:20 +0700

    update infinite slide

--- src/component/InfiniteSlider/index.tsx
+++ src/component/InfiniteSlider/index.tsx
@@ -1,48 +1,59 @@
-import React, {FC, useEffect, useRef} from 'react';
+import React, {FC, useRef} from 'react';
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
 import styles from "./infiniteSlider.module.scss";
 
 type InfiniteSliderProps = {
-    speed: number;
-    items: React.ReactNode[];
+	speed: number;
+	items: React.ReactNode[];
 }
 
-const InfiniteSlider: FC<InfiniteSliderProps> = ({
-    speed,
-    items
-}) => {
-
-    const ref = useRef<HTMLDivElement>(null);
-
-    useGSAP(() => {
-        const loop = ref.current;
-        if (!loop) return;
-        const firstChild = loop.children[0];
-        loop.appendChild(firstChild.cloneNode(true));
-        loop.appendChild(firstChild.cloneNode(true));
-
-        const infiniteTimeline = gsap.timeline({ repeat: Infinity });
-        infiniteTimeline
-            .to(loop, speed, { ease: 'none', xPercent: -100 })
-            .to(loop, 0, { ease: 'none', x: 0 });
-    })
-
-    return (
-        <div className={styles["infinite-slider-wrapper"]}>
-            <div className={styles["infinite-slider"]} ref={ref}>
-                <div className={styles["infinite-slider-items"]}>
-                    {
-                        items.map((item, index) => {
-                            return (
-                                <div key={index}>{item}</div>
-                            )
-                        })
-                    }
-                </div>
-            </div>
-        </div>
-    );
+const InfiniteSlider: FC<InfiniteSliderProps> = ({speed, items}) => {
+	
+	const sliderRef = useRef<HTMLDivElement>(null);
+	const timelineRef = useRef<GSAPTimeline>();
+	
+	useGSAP(() => {
+		const loop = sliderRef.current;
+		if (!loop) return;
+		const firstChild = loop.children[0];
+		loop.appendChild(firstChild.cloneNode(true));
+		loop.appendChild(firstChild.cloneNode(true));
+		
+		timelineRef.current = gsap.timeline({repeat: Infinity});
+		timelineRef.current
+			.to(loop, {ease: 'none', xPercent: -100, duration: speed})
+			.to(loop, {ease: 'none', x: 0, duration: 0});
+	})
+	
+	const handleEnterItem = () => {
+		timelineRef.current?.pause();
+	}
+	const handleLeaveItem = () => {
+		timelineRef.current?.resume();
+	}
+	
+	return (
+		<div className={styles["infinite-slider-wrapper"]}>
+			<div
+				className={styles["infinite-slider"]}
+				ref={sliderRef}
+				onPointerEnter={handleEnterItem}
+				onMouseLeave={handleLeaveItem}>
+				<div className={styles["infinite-slider-items"]}>
+					{
+						items.map((item, index) => {
+							return (
+								<div key={index}>
+									{item}
+								</div>
+							)
+						})
+					}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default InfiniteSlider;
