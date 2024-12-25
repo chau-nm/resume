dbe7f39d0186055f1f8474d030a9d25963179cdb
Nguyễn Minh Châu <chaunm.146.2001@gmail.com>
2024-12-25 23:58:24 +0700

    update cv content

--- src/component/CurriculumVitae/Content/ProfileSummary/index.tsx
+++ src/component/CurriculumVitae/Content/ProfileSummary/index.tsx
@@ -5,8 +5,12 @@ export const ProfileSummary: FC = () => {
 	return (
 		<Section title={"Profile Summary"}>
 			<p>
-				Dynamic and passionate software developer with 2 years of experience in designing
-				and implementing robust backend solutions.
+				Dynamic and passionate software developer with 2 years of experience in designing and implementing robust backend solutions.
+				Proficient in programming languages including Java, JavaScript, and TypeScript, with a deep understanding of frameworks such as Node.js and Spring.
+				Experienced in working with databases like MySQL and PostgreSQL.
+				Additionally, I have hands-on experience with frontend frameworks like ReactJS and NextJS, enabling me to create seamless and engaging user experiences.
+				Demonstrated ability to collaborate effectively in team environments, delivering high-quality software on time.
+				Committed to continuous learning and staying abreast of industry trends, I am eager to contribute to innovative projects.
 			</p>
 		</Section>
 	)
