a06369861d19e93f1e7804f1f7ba227a1bbb7b21
Nguyễn Minh Châu <chaunm.146.2001@gmail.com>
2024-12-26 23:32:11 +0700

    update format code

--- src/feature/main/Skill/TechnicalSlider/TechnicalBlock.tsx
+++ src/feature/main/Skill/TechnicalSlider/TechnicalBlock.tsx
@@ -1,21 +1,21 @@
-import { FC } from "react";
+import {FC} from "react";
 import styles from "./technicalSlider.module.scss";
 
 type TechnicalBlockProps = {
-    imageUrl: string;
-    caption: string;
+	imageUrl: string;
+	caption: string;
 }
 
 export const TechnicalBlock: FC<TechnicalBlockProps> = ({
-    imageUrl,
-    caption
+  imageUrl,
+  caption
 }) => {
-    return (
-        <div className={styles.block}>
-            <div className={styles["img-wrapper"]}>
-                <img src={imageUrl} alt={caption}/>
-            </div>
-            <span>{caption}</span>
-        </div>
-    )
+	return (
+		<div className={styles.block}>
+			<div className={styles["img-wrapper"]}>
+				<img src={imageUrl} alt={caption}/>
+			</div>
+			<span>{caption}</span>
+		</div>
+	)
 }
--- src/feature/main/Skill/TechnicalSlider/technicalData.ts
+++ src/feature/main/Skill/TechnicalSlider/technicalData.ts
@@ -1,50 +1,50 @@
 import {
-    CSSSkill, 
-    DockerSkill,
-    HTMLSkill,
-    JavaScriptSkill,
-    JavaSkill,
-    MySQLLogo, 
-    PostgreSkill,
-    PythonSkill,
-    TypeScriptLogo
+	CSSSkill,
+	DockerSkill,
+	HTMLSkill,
+	JavaScriptSkill,
+	JavaSkill,
+	MySQLLogo,
+	PostgreSkill,
+	PythonSkill,
+	TypeScriptLogo
 } from "../../../../assets/images/logo";
 
 export const technicalData = [
-    {
-        imageUrl: HTMLSkill,
-        caption: "HTML"
-    },
-    {
-        imageUrl: CSSSkill,
-        caption: "CSS"
-    },
-    {
-        imageUrl: JavaScriptSkill,
-        caption: "Javascript"
-    },
-    {
-        imageUrl: TypeScriptLogo,
-        caption: "Typescript"
-    },
-    {
-        imageUrl: JavaSkill,
-        caption: "Java"
-    },
-    {
-        imageUrl: PythonSkill,
-        caption: "Python"
-    },
-    {
-        imageUrl: MySQLLogo,
-        caption: "MySQL"
-    },
-    {
-        imageUrl: PostgreSkill,
-        caption: "Postgres"
-    },
-    {
-        imageUrl: DockerSkill,
-        caption: "Docker"
-    },
+	{
+		imageUrl: HTMLSkill,
+		caption: "HTML"
+	},
+	{
+		imageUrl: CSSSkill,
+		caption: "CSS"
+	},
+	{
+		imageUrl: JavaScriptSkill,
+		caption: "Javascript"
+	},
+	{
+		imageUrl: TypeScriptLogo,
+		caption: "Typescript"
+	},
+	{
+		imageUrl: JavaSkill,
+		caption: "Java"
+	},
+	{
+		imageUrl: PythonSkill,
+		caption: "Python"
+	},
+	{
+		imageUrl: MySQLLogo,
+		caption: "MySQL"
+	},
+	{
+		imageUrl: PostgreSkill,
+		caption: "Postgres"
+	},
+	{
+		imageUrl: DockerSkill,
+		caption: "Docker"
+	},
 ]
