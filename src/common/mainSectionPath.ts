d95ac06fbf2b07a2449e1ef01757103116ccff5b
Nguyễn Minh Châu <chaunm.146.2001@gmail.com>
2024-12-27 23:52:04 +0700

    update background home

--- src/common/mainSectionPath.ts
+++ src/common/mainSectionPath.ts
@@ -3,4 +3,5 @@ export const HOME = "/home";
 export const SUMMARY = "/summary";
 export const EXPERIENCE = "/experience";
 export const ALBUM = "/album";
+export const SKILL = "/skill";
 export const PROJECT = "/repository";
