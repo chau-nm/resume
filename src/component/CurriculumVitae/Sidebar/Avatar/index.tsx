22f7716a44d39168d85ece0d58903eba2747d54b
Nguyễn Minh Châu <chaunm.146.2001@gmail.com>
2024-12-27 22:44:09 +0700

    update avatar on cv

--- src/component/CurriculumVitae/Sidebar/Avatar/index.tsx
+++ src/component/CurriculumVitae/Sidebar/Avatar/index.tsx
@@ -1,10 +1,11 @@
 import {FC} from "react";
+import avatar from "assets/images/avatar.png";
 import styles from "./avatar.module.scss";
 
 export const Avatar: FC = () => {
 	return (
 		<div className={styles.avatar}>
-			<img src={""} alt={""}/>
+			<img src={avatar} alt={""} width={"100%"}/>
 		</div>
 	)
 }
