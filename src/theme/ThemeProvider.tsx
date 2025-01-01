449d2525899b7f5602dca0cad342e791413f02d3
Nguyễn Minh Châu <chaunm.146.2001@gmail.com>
2025-01-01 23:59:04 +0700

    update save theme to local storage

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,10 +1,21 @@
-import {FC, PropsWithChildren, useState} from "react";
+import {FC, PropsWithChildren, useEffect, useState} from "react";
 import {ThemeEnum} from "./themeEnum";
 import {ThemeContext} from "./ThemeContext";
 import "./sass/theme.scss";
 
 const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
-    const [theme, setTheme] = useState<ThemeEnum>(ThemeEnum.DARK);
+    const [theme, setTheme] = useState<ThemeEnum>(() => {
+      const theme = localStorage.getItem("theme");
+      if (!Object.values(ThemeEnum).includes(theme as ThemeEnum)) {
+        return ThemeEnum.DARK;
+      } else {
+        return theme as ThemeEnum;
+      }
+    });
+  
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
     
     return (
         <ThemeContext.Provider value={{
