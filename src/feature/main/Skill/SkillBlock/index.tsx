42a3cba833579697a24bf9df5dbb38af14be77d9
Nguyễn Minh Châu <chaunm.146.2001@gmail.com>
2025-01-03 18:56:53 +0700

    update light

--- src/feature/main/Skill/SkillBlock/index.tsx
+++ src/feature/main/Skill/SkillBlock/index.tsx
@@ -1,9 +1,11 @@
-import {FC, useRef} from "react";
+import {FC, useContext, useRef} from "react";
 import styles from "./skillBlock.module.scss";
 import {useGSAP} from "@gsap/react";
-import {tweenFromToScroll} from "libs/gsap";
+import {timelineScroll} from "libs/gsap";
 import classNames from "classnames";
 import {useTranslation} from "react-i18next";
+import {ThemeContext} from "../../../../theme/ThemeContext";
+import {ThemeEnum} from "../../../../theme/themeEnum";
 
 type SkillBlockProps = {
 	title: string;
@@ -21,34 +23,50 @@ export const SkillBlock: FC<SkillBlockProps> = ({
   isShowMore
 }) => {
 	const {t} = useTranslation();
+	const {theme} = useContext(ThemeContext);
 	const ref = useRef<HTMLDivElement | null>(null);
+	const lightRef = useRef<HTMLDivElement | null>(null);
 	const contentRef = useRef<HTMLDivElement | null>(null);
 	
 	useGSAP(() => {
-		tweenFromToScroll(
+		const timeline = timelineScroll(ref.current);
+		timeline.fromTo(
 			ref.current,
 			{x: 50 * (position ? -1 : 1), opacity: 0},
 			{x: 0, opacity: 1, duration: 1}
-		);
+		)
+		if (lightRef) {
+			timeline.fromTo(
+				lightRef.current,
+				{opacity: 0},
+				{opacity: 1}
+			);
+		}
 	})
 	
 	return (
 		<div ref={ref} className={styles.block}>
-			<div className={styles.title}>
-				<h3>{title}</h3>
-			</div>
-			<div ref={contentRef} className={classNames(styles.content, {[styles.toggle]: isShowMore}) }>
-				<p>{content}</p>
-			</div>
-			<div
-				className={styles["toggle-button"]}
-				onClick={toggle}
-			>
-				{
-					!isShowMore
-						? <span>{t("skill.showMore")}</span>
-						: <span>{t("skill.showLess")}</span>
-				}
+			{
+				theme === ThemeEnum.DARK &&
+				<div ref={lightRef} className={classNames(styles.light, {[styles.reverse]: position})}></div>
+			}
+			<div className={styles.layer}>
+				<div className={styles.title}>
+					<h3>{title}</h3>
+				</div>
+				<div ref={contentRef} className={classNames(styles.content, {[styles.toggle]: isShowMore})}>
+					<p>{content}</p>
+				</div>
+				<div
+					className={styles["toggle-button"]}
+					onClick={toggle}
+				>
+					{
+						!isShowMore
+							? <span>{t("skill.showMore")}</span>
+							: <span>{t("skill.showLess")}</span>
+					}
+				</div>
 			</div>
 		</div>
 	)
