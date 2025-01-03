906e459704f211a66b69e0db5b88df20bd3ff6d9
Nguyễn Minh Châu <chaunm.146.2001@gmail.com>
2025-01-03 11:33:01 +0700

    update footer zalo contact

--- src/feature/main/Footer/ContactLink.tsx
+++ src/feature/main/Footer/ContactLink.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import styles from "./footer.module.scss";
-import { FacebookOutLine, GithubOutLine, LinkedInOutline } from "assets/icons";
+import {FacebookOutLine, GithubOutLine, LinkedInOutline, ZaloOutline} from "assets/icons";
 
 export const ContactLink: FC = () => (
   <div className={styles["contact-link"]}>
@@ -33,6 +33,16 @@ export const ContactLink: FC = () => (
           className="linkedin"
         >
           <LinkedInOutline />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://zalo.me/0865703869"
+          target="_blank"
+          rel="noreferrer"
+          className="zalo"
+        >
+          <ZaloOutline />
         </a>
       </li>
     </ul>
